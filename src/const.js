a4d2214f977aac6aa55dd96f13ca2be707e8d989
Grazhinka <gr-km@mail.ru>
2023-02-27 20:27:59 +0300

    api

--- src/const.js
+++ src/const.js
@@ -1,2 +1,4 @@
-export const API_URI='http://localhost:3024';
+export const API_URI='https://innovative-dawn-oviraptor.glitch.me';
+//export const API_URI='http://localhost:3024';
+
 export const POCTFIX='/api/product';
